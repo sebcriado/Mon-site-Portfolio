723cd1aedcf34710b9f5251c9b250b9ddfecfd5e
sebcriado <contact.sebastiencriado@gmail.com>
2023-01-03 16:09:51 +0100

    version finale accueil et V1 Projets

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const btn = document.querySelector(".navbar-toggler");
 const menu = document.querySelector(".search-menu");
 const crossBtn = document.querySelector(".cross");
+const buttons = document.querySelector(".nav-link");
 
 btn.addEventListener("click", () => {
   menu.style.transform = "translateY(0%)";
+  menu.classList.remove("hidden");
+  menu.style.transition = "0.5s";
 });
 
 crossBtn.addEventListener("click", () => {
   menu.style.transform = "translateY(-100%)";
+});
+
+buttons.addEventListener("click", () => {
+  menu.classList.add("hidden");
 });
