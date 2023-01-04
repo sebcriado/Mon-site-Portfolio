cc62858919af98439d1d2ac46d2ef3b5fe71e0a0
sebcriado <contact.sebastiencriado@gmail.com>
2023-01-04 11:58:26 +0100

    V1 contact

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const btn = document.querySelector(".navbar-toggler");
 const menu = document.querySelector(".search-menu");
 const crossBtn = document.querySelector(".cross");
 const buttons = document.querySelector(".nav-link");
+const API_KEY = "wJk7lutnfSTxbkNPYG5fJIeEYbWx9v76L65GSWyg";
+const API_ENDPOINT = "https://api.nasa.gov/planetary/apod";
+
+// NAVBAR
 
 btn.addEventListener("click", () => {
   menu.style.transform = "translateY(0%)";
@@ -16,3 +20,25 @@ crossBtn.addEventListener("click", () => {
 buttons.addEventListener("click", () => {
   menu.classList.add("hidden");
 });
+
+// API NASA
+
+async function getNasaImage() {
+  try {
+    const response = await fetch(`${API_ENDPOINT}?api_key=${API_KEY}`);
+    const data = await response.json();
+    const imageUrl = data.url;
+    const nameUrl = data.copyright;
+    const titleUrl = data.title;
+    const titleElement = document.querySelector(".text-url");
+    const nameElement = document.querySelector(".name-url");
+    const imgElement = document.querySelector(".space");
+    imgElement.src = imageUrl;
+    nameElement.innerHTML = nameUrl;
+    titleElement.innerHTML = titleUrl;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getNasaImage();
